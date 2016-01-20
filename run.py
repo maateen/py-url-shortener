556afa3ee2811f5b2f0acebed0c6772733c66f10
Maksudur Rahman Maateen <ugcoderbd@gmail.com>
2016-01-20 22:35:55 +0600

    Commenting out from flask_admin import Admin

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_admin import Admin
+#from flask_admin import Admin
 
 app = Flask(__name__)
 app.config.from_object('config')
