00eb76911f95b55fd22a20447947157ec133a7e6
Maksudur Rahman Maateen <ugcoderbd@gmail.com>
2016-01-20 22:47:57 +0600

    Changing structure

--- run.py
+++ run.py
@@ -1,10 +1,4 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-#from flask_admin import Admin
-
-app = Flask(__name__)
-app.config.from_object('config')
-db = SQLAlchemy(app)
+from app import app
 
 if __name__=='__main__':
 	app.run(port=5000,debug=True)
