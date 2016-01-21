0392115e3737049fd358ae7b93b8d3fc992369db
Maksudur Rahman Maateen <ugcoderbd@gmail.com>
2016-01-21 23:58:30 +0600

    Removing SERVER_NAME

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://shortener:123456@localhost/shortener'
-SQLALCHEMY_ECHO = True
-SQLALCHEMY_TRACK_MODIFICATIONS = True
-CSRF_ENABLED = True
-SECRET_KEY = "xckdfuhaskdhihnlqkCDHKLruqILH"
-DEBUG = True
-SERVER_NAME = '127.0.0.1:5000'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://shortener:123456@localhost/shortener' #Set up your MySQL/MariaDB coonection - mysql+pymysql://username:password@host/database.
+SECRET_KEY = "Set a long string that can't be guessed easily"
+SQLALCHEMY_ECHO = True #This will show SQLAlchemy output.
+SQLALCHEMY_TRACK_MODIFICATIONS = True #This will show SQLAlchemy track modification output.
+CSRF_ENABLED = True #Don't make it False.
+DEBUG = True #Make it false to turn the debugger off.
