9f5dedb1aa50bf4af964be352a449e9ecc080084
Maksudur Rahman Maateen <ugcoderbd@gmail.com>
2016-01-21 14:01:58 +0600

    Adding DEBUG and WTF_CSRF parameters

--- config.py
+++ config.py
@@ -1,2 +1,6 @@
 SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://shortener:123456@localhost/shortener'
 SQLALCHEMY_ECHO = True
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+WTF_CSRF_ENABLED = True
+WTF_CSRF_SECRET_KEY = "somethingimpossibletoguess"
+DEBUG = True
