ecc789312d620ff640d129d818a91caaf3fee396
Maksudur Rahman Maateen <ugcoderbd@gmail.com>
2016-01-22 22:52:34 +0600

    Sorting imports and Doing Documentation following PEP8 convention.

--- app/api.py
+++ app/api.py
@@ -1,7 +1,16 @@
-from app import app
-from flask_restful import Resource, Api
+from app.utils import short_url_generator
+from app.utils import url_checker
+from flask import request
 
-api = Api(app)
 
-class ApiV10(Resource):
-	def post(self):
+def apiv10(long_url):
+    # This is our Version-1.0 API function. It will take the long_url as
+    # parameter then return two different dictionary based on url_checker
+    # function output.
+    checker = url_checker(long_url)
+    if checker is True:
+        short_url = short_url_generator(long_url)
+        return {'short_url': short_url}
+    else:
+        error = "The URL seems to be dead at this moment."
+        return {'error': error}
